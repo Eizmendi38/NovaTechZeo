4d8a5a05fc6fb889217acfeddcec5b2e9d57f6e5
Odei <odei.cebanc@gmail.com>
2025-09-22 11:10:10 +0200

    MenuUsuario

---
+++ ScrumMaster/src/aplicacion/MenuUsuario.java
@@ -0,0 +1,10 @@
+package aplicacion;
+
+import java.util.*;
+import obj.Firmas;
+
+public class MenuUsuario{
+    public static void ejecutar(ArrayList<Firmas> firmList, Scanner scanner, Random random) {
+        int opcion;
+    }
+}
