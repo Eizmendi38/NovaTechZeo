d56bb7ad7e316da9c3e6bca8fd915987c99695fa
Eizmendi38 <eizmendiiker8@gmail.com>
2025-09-22 10:28:46 +0200

    Métodos getters y setters y algunos y comentarios del programa

--- ScrumMaster/src/obj/Firmas.java
+++ ScrumMaster/src/obj/Firmas.java
@@ -3,13 +3,37 @@ package obj;
 import java.time.LocalDateTime;
 
 public class Firmas {
+
+    //Declarar atributos
     private int id;
     private String firma;
     private LocalDateTime horaFirmada;
 
+
+    // Constructor
     public Firmas (int id, String firma) {
         this.id = id;
         this.firma = firma;
         this.horaFirmada = LocalDateTime.now();
+    }
+
+    //Getters y Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirma() {
+        return firma;
+    }
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+    public LocalDateTime getHoraFirmada() {
+        return horaFirmada;
+    }
+    public void setHoraFirmada(LocalDateTime horaFirmada) {
+        this.horaFirmada = horaFirmada;
     }
 }
