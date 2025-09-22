3a600933c54daa2efc9dff8d68dd6dca02655c63
Eizmendi38 <eizmendiiker8@gmail.com>
2025-09-22 11:13:08 +0200

    Update MenuAdmin.java

--- ScrumMaster/src/aplicacion/MenuAdmin.java
+++ ScrumMaster/src/aplicacion/MenuAdmin.java
@@ -2,3 +2,29 @@ package aplicacion;
 
 import obj.Firmas;
 import java.util.*;
+
+public class MenuAdmin {
+    public static void ejecutar(ArrayList<Firmas> firmList, Scanner scanner, Random random) {
+        int opcion;
+        do {
+            Menu.mostrarMenuAdmin();
+            opcion = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Lista de todas las firmas:");
+                    for (Firmas firma : firmList) {
+                        System.out.println(firma);
+                    }
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("Saliendo del sistema...");
+                    return;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        } while (true);
+    }
+}
