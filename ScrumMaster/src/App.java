df5cc46e5c569026721c14f760f8448ad3ae0713
Gari <garikworkaddress@gmail.com>
2025-09-22 10:26:06 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- ScrumMaster/src/App.java
+++ ScrumMaster/src/App.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 import java.util.Random;
+import obj.Firmas;
 
 public class App {
     public static void main(String[] args){
         Random r= new Random();
+        ArrayList<Firmas> firmList = new ArrayList<>();
         
+        System.out.println("Bienvenido al sistema de firmas.\n¿Deseas acceder al menú de ");
     }
 }
